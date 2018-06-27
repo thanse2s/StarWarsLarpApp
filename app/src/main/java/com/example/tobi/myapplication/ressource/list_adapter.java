6f3f508098953e1d2189cd05dec235a876d23667
Tohan0510 <tohan0510@gmail.com>
2018-06-27 12:29:56 +0200

    List View with Imager/String implimented (Incompled)

--- app/src/main/java/com/example/tobi/myapplication/ressource/list_adapter.java
+++ app/src/main/java/com/example/tobi/myapplication/ressource/list_adapter.java
@@ -2,12 +2,14 @@ package com.example.tobi.myapplication.ressource;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.tobi.myapplication.R;
 import com.example.tobi.myapplication.ressource.data_file;
 
 import org.w3c.dom.Text;
@@ -18,6 +20,7 @@ import java.util.ArrayList;
 public class list_adapter extends BaseAdapter {
 
     private  Context mContext;
+    private LayoutInflater layoutInflater;
 
     public list_adapter(Context context){
         this.mContext=context;
@@ -41,10 +44,36 @@ public class list_adapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        TextView txt = new TextView(mContext);
-        txt.setText(String.valueOf(data_file.get_name_from_list_Item(position)));
-        txt.setTextColor(Color.WHITE);
-        return  txt;
+       ViewHolder holder;
+       if(convertView==null) {
+           convertView = layoutInflater.inflate(R.layout.grid_item, null);
+           holder = new ViewHolder();
+           holder.flagView = (ImageView) convertView.findViewById(R.id.im_view);
+           holder.nameView = (TextView) convertView.findViewById(R.id.im_text);
+           convertView.setTag(holder);
+       }
+       else
+       {
+           holder=(ViewHolder)convertView.getTag();
+       }
 
+        holder.nameView.setText(data_file.get_name_from_list_Item(position).toString());
+        int imageId = this.getMipmapResIdByName();
+        holder.flagView.setImageResource(imageId);
+        return convertView;
+    }
+
+    public int getMipmapResIdByName(){
+        String pkgName = mContext.getPackageName();
+
+        int resetID = mContext.getResources().getIdentifier("file","mipmap",pkgName);
+                return resetID;
+    }
+
+
+    static class ViewHolder{
+
+        ImageView flagView;
+        TextView nameView;
     }
 }
