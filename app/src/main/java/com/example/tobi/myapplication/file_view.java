8cba833ba08bb2ea5bad831c250b0cc82252f439
Tohan0510 <tohan0510@gmail.com>
2018-06-27 10:58:39 +0200

    j

---
+++ app/src/main/java/com/example/tobi/myapplication/file_view.java
@@ -0,0 +1,35 @@
+package com.example.tobi.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import com.example.tobi.myapplication.ressource.data_file;
+import com.example.tobi.myapplication.ressource.list_adapter;
+
+public class file_view extends Fragment {
+
+    public file_view(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view =   inflater.inflate(R.layout.files_grid_view, container, false);
+        super.onCreate(savedInstanceState);
+
+        GridView gridView = (GridView)getActivity().findViewById(R.id.file_grid);
+        list_adapter adp = new list_adapter(getActivity());
+        gridView.setAdapter(adp);
+
+        return  view;
+
+    }
+
+
+
+}
